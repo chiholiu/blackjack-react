12fd346ba2de65f39649b8a877a094a0fd56a97b
Chi Ho <chiho@chis-air.home>
2018-09-12 16:03:24 +0200

    added player score calculator

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 let array = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
+let result = 0;
 
 // const suits = ['♣', '♦', '♥', '♠'];
 // const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
@@ -11,18 +12,21 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
+
       random: null,
       player: [],
       computer: [],
       card: null,
       playerScore: 0,
+      playerScoreArray: [],
       computerScore: 0
     };
     
     this.getCard = this.getCard.bind(this);
     this.shuffle = this.shuffle.bind(this);
     this.checkScore = this.checkScore.bind(this);
-    // this.getCardValue = this.getCardValue.bind(this);
+    this.calculateScore = this.calculateScore.bind(this);
+    this.reset = this.reset.bind(this);
   }
 
   componentDidMount() {
@@ -35,11 +39,21 @@ class App extends React.Component {
     const playerScore = playerArray;
     const newArray = playerScore.map(card => this.getCardValue(card));
     const reducer = (accumulator, currentValue) => accumulator + currentValue;
-    const result = newArray.reduce(reducer);
+    result = newArray.reduce(reducer);
     this.setState({
       playerScore: result
     })
-    // return result;
+  }
+
+  calculateScore() {
+    if( result < 21 ) {
+      this.state.playerScoreArray.push(result);
+      this.setState({
+        player: []
+      });
+    } else {
+      this.state.playerScoreArray.push(result + (-result + 10));
+    }
   }
 
   getCardValue (card) {
@@ -70,6 +84,13 @@ class App extends React.Component {
     }
   }
 
+  reset() {
+    this.setState({
+      computer: [],
+      player: []
+    })
+  }
+
   shuffleCard() {
     const rand = array[Math.floor(array.length * Math.random())];
     this.state.computer.length < 2 ? this.state.computer.push(rand) : this.state.player.push(rand);
@@ -97,11 +118,13 @@ class App extends React.Component {
         </div>
 
         <div>
+          <button onClick={this.calculateScore}>Fold</button>
+        </div>
+
+        <div>
           <p>Player Score</p>
           <span>{this.state.playerScore}</span>
         </div>
-
-
       </div>
     );
   }
