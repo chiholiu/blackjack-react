7c46faf697974b76648660abdb5cccdbaf1f22b7
Chi Ho <>
2018-09-15 22:16:17 +0200

    compare computer player score

--- src/App.js
+++ src/App.js
@@ -15,14 +15,12 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
+      shuffleButton: true,
       random: null,
       player: [],
       computer: [],
-      card: null,
       playerScore: 0,
       computerScore: 0,
-      playerScoreArray: [],
-      computerScoreArray: [],
       totalPlayerScore: 0
     };
     
@@ -34,28 +32,20 @@ class App extends React.Component {
     this.reset = this.reset.bind(this);
     this.checkBothCards = this.checkBothCards.bind(this);
     this.recalculateComputerScore = this.recalculateComputerScore.bind(this);
+    this.checkWinner = this.checkWinner.bind(this);
   }
 
-  // componentDidMount() {
-  //   window.addEventListener('load', function() {
-  //     console.log('loaded successfully ');
-  //   });
-  // }
+  componentDidMount() {
+    window.addEventListener('load', function() {
+      console.log('loaded successfully ');
+    });
+  }
 
   getCard() {
     const rand = array[Math.floor(array.length * Math.random())];
     this.setState({
       player: [...this.state.player, rand]
-    }, () => this.checkScore(this.state.player, playerScore));
-    console.log(result);
-    if(result > 21) {
-      this.calculateScore();
-    } else if (result === 21) {
-      console.log('black jack dude');
-    } else {
-      // this.calculateScore();
-      console.log('still in the game');
-    }
+    }, () => this.checkScore(this.state.player, playerScore)); 
   }
 
   shuffle() {
@@ -68,7 +58,7 @@ class App extends React.Component {
     const rand = array[Math.floor(array.length * Math.random())];
     this.setState({
       computer: [...this.state.computer, rand]
-    }, () => this.recalculateComputerScore(this.state.computer, computerScore));
+    }, () => this.checkScore(this.state.computer, computerScore));
   }
 
   recalculateComputerScore(element, keyValue) {
@@ -77,7 +67,11 @@ class App extends React.Component {
     let totalComputerScore = newComputerArray.reduce(reducer, 0);
     this.setState({
       [keyValue]: totalComputerScore
-    }, () => this.checkBothCards());
+    });
+    console.log('keyValue ' + keyValue);
+    if(keyValue == computerScore) {
+      this.checkBothCards();
+    }
   }
 
   shuffleCard(n) {
@@ -94,8 +88,10 @@ class App extends React.Component {
           this.shuffleCard(n-1);
         });
       }
+      this.setState({
+        shuffleButton: false
+      })
     }
-    document.getElementById('shuffleButton').setAttribute('disabled', '');
   }
 
   checkScore(theArray, keyName) {
@@ -105,45 +101,60 @@ class App extends React.Component {
     this.setState({
       [keyName]: result
     });
-    console.log(this.state.computerScore);
-    console.log(this.state.playerScore);
   }
 
   calculateScore() {
     if(result < 21) {
       this.setState({
-        playerScoreArray: [...this.state.playerScoreArray, Math.round(result + (result * 1.2))]
-      });
+        player: [...this.state.player, Math.round(result + (result * 1.2))]
+      }, () => this.calculateTotalScore(this.state.player));
     } else {
       this.setState({
-        playerScoreArray: [...this.state.playerScoreArray, Math.round(result - (result * 1.2))]
-      });
+        player: [...this.state.player, Math.round(result - (result * 1.2))]
+      }, () => this.calculateTotalScore(this.state.player), console.log(this.state.player));
     }
-    this.calculateTotalScore(this.state.playerScoreArray);
-    this.setState({
-      player: [],
-      computer: []
-    });
   }
 
-  calculateTotalScore(element) {
-    const playerScore = element;
-    const newPlayerArray = playerScore.map(card => card);
+  calculateTotalScore(playerScore) {
+    const newPlayerArray = playerScore.map(card => this.getCardValue (card));
     const reducer = (accumulator, currentValue) => accumulator + currentValue;
     totalPlayerScore = newPlayerArray.reduce(reducer, 0);
     this.setState({
-      totalPlayerScore: totalPlayerScore
-    })
+      totalPlayerScore: this.state.totalPlayerScore + totalPlayerScore,
+    }, () => this.reset())
   }
 
   checkBothCards() {
-    console.log(this.state.computerScore);
-    if(this.state.computerScore < 17) {
+    console.log('called');
+    if(this.state.computerScore < 17 && this.state.playerScore <= 21) {
+      console.log('smaller than 17');
       this.shuffleComputerCard();
     } else if (this.state.computerScore > 17 && this.state.computerScore < 21  && this.state.playerScore <= 21) {
-      console.log('check winner');
+      this.checkWinner();
+      console.log('between 17 and 21');
     } else {
-      return;
+      console.log('you beat computer');
+      this.checkWinner();
+    }
+  }
+
+  checkWinner() {
+    this.setState({
+      shuffleButton: true
+    })
+    if(this.state.playerScore > this.state.computerScore) {
+      this.setState({
+        player: [...this.state.player, Math.round(result + (result * 1.2))]
+      }, () => this.calculateTotalScore(this.state.player));
+    } else if (this.state.playerScore < this.state.computerScore) {
+      this.setState({
+        player: [...this.state.player, Math.round(result + (result * 1.2))]
+      }, () => this.calculateTotalScore(this.state.player));
+    } else {
+      this.setState({
+        player: [...this.state.player, Math.round(result + (result * 1))]
+      }, () => this.calculateTotalScore(this.state.player));
+        console.log('equal');
     }
   }
 
@@ -161,12 +172,10 @@ class App extends React.Component {
   }
 
   reset() {
-    return function() {
       this.setState({
         computer: [],
         player: []
       })
-    }
   }
 
   render() {
@@ -174,7 +183,7 @@ class App extends React.Component {
       <div>
         <div className="App">
           <button onClick={this.getCard}>Play</button>
-          <button id="shuffleButton" onClick={this.shuffle}>Shuffle</button>
+          <button onClick={this.shuffle} disabled={!this.state.shuffleButton}>Shuffle</button>
         </div>
 
         <div>
@@ -194,6 +203,12 @@ class App extends React.Component {
           <p>Player Score</p>
           <span>{this.state.playerScore}</span>
         </div>
+
+        {/* <div>
+          <p>Computer Score</p>
+          
+          <span>{this.state.computerScore}</span>
+        </div> */}
 
         <div>{this.state.totalPlayerScore}</div>
       </div>
